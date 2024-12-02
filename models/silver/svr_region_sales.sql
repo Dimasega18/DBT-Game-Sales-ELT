76e7049793143905642086c50e760b83e72fe83c
Dimasega18 <dimasega1811@gmail.com>
2024-12-03 00:54:27 +0700

    add region_sales_id

--- models/silver/svr_region_sales.sql
+++ models/silver/svr_region_sales.sql
@@ -1,6 +1,7 @@
 {{ config(schema='silver',materialized='table') }}
 
 SELECT 
+    ROW_NUMBER() OVER(order by rs.region_id) AS region_sales_id,
     rs.region_id,
     r.region_name,
     rs.game_platform_id,
